c594652c1b943cc53db1be8b2b4b25823d242c99
Informatica Kinal <informatica@ALUMNOS.local>
2020-02-14 13:46:26 -0600

    Initial Config

---
+++ Index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const Express = require('express');
+const Mongoose = require('mongoose');
+
+const App = require('./App');
+
+const PORT = '3500';
+
+Mongoose.Promise = global.Promise;
+
+Mongoose.connect('mongodb://127.0.0.1:27017/OrganizationSystem', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+    console.log('Conexion a base de datos satisfactoria');
+    App.listen(PORT, () => {console.log(`Servidor abierto en puerto '${PORT}'`)});
+})
+.catch((err) => {console.log('Conexion Erronea', err)});
